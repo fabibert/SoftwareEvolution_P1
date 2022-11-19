82dd3eef1ef275f4b75c137fea199f017979e7b7
fabibert <76185888+fabibert@users.noreply.github.com>
2022-11-19 23:03:50 +0100

    cleaner

--- quarry.py
+++ quarry.py
@@ -52,58 +52,13 @@ def get_modified_files(path: str):
     header_column = []
     for k, v in data.items():
         header_column.append(k)
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(header_column)
+
     i = 0
-    #[list.insert(0, header_column[++i]) for list in matrix]
     for list in matrix:
         list.insert(0, header_column[i])
         i = i+1
 
-
-    #matrix[0].insert(0, header_column)
-
     print(matrix)
-
-    #matrix_names = [[k for k, v in data.items()],[[y for x, y in v.items()] for k, v in data.items()]]
-
-    #matrix_names2 = [[y for x, y in v.items()] for k, v in data.items()]
-    # for i in range(len(matrix_names2)):
-    #     for k, v in data.items():
-    #         matrix_names2[i].insert(0, k)
-
-    #for k, v in data.items():
-
-    #[{try: list.insert(0, "hallo") except: list+1} for list in matrix_names2]
-
-    #print(matrix_names2)
-
-    matrix_names3 = [[[k , [y for x,y in v.items()]] for k,v in data.items()]]
-    #print(matrix_names3)
-
-
-
-
-
-    # people = {1: {'Name': 'John', 'Age': '27', 'Sex': 'Male'},
-    #           2: {'Name': 'Marie', 'Age': '22', 'Sex': 'Female'}}
-    #
-    # matrix = [][]
-    # for p_id, p_info in people.items():
-    #     print("\nPerson ID:", p_id)
-    #
-    #     for key in p_info:
-    #         print(key + ':', p_info[key])
-
-
-
-    # matrixed_data = []
-    # #for entry in list
-    # for entry in listed_data():
-    #     for k, v in entry.items():
-    #         matrixed_data.append([k, v])
-    # print(matrixed_data)
-
 
     with open('change_log.csv', 'w', encoding='UTF8', newline='') as f:
          writer = csv.writer(f)
