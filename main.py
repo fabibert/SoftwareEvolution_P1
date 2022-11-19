47477dd3af1ff89cf37e23ea86ec7aaf991d9ae4
fabibert <76185888+fabibert@users.noreply.github.com>
2022-11-19 22:57:45 +0100

    draft

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from quarry import get_modified_files
 from quarry import get_change_numbers
 
 
+
 if __name__ == '__main__':
 
-    path = "https://github.com/Xyarlo/SoPra-Server.git"
+    path = "https://github.com/lutzidan/assignment1-sw-construction.git"
 
     get_change_numbers(path)
     get_modified_files(path)
+
+
--- quarry.py
+++ quarry.py
@@ -1,6 +1,8 @@
 from pydriller import *
 from pydriller import Repository
 import csv
+import pandas
+import matplotlib as plt
 
 ###deprecated
 def old_get_modified_files(path: str):
@@ -24,23 +26,100 @@ def old_get_modified_files(path: str):
 
 
 def get_modified_files(path: str):
-    header = ['commit', 'changed_files']
+    #header = ['commit', 'changed_files']
+    #header = ['filename']
     data = {}
 
     for commit in Repository(path).traverse_commits():
         for file in commit.modified_files:
             if data.get(file.filename) is None:
                 data[file.filename] = {}
-            data[file.filename][commit.hash] = _count_lines(str(file.content))
+            data[file.filename][commit.hash] = _count_lines(str(file.source_code)) #nested dictionary
 
-    listed_data = []
+    # matrixed_data = []
+    # index_counter=0
+    # for k, v in data.items():
+    #     matrixed_data.append(k)
+    #     matrixed_data.append([])
+    #     for x,y in v.items():
+    #         matrixed_data[index_counter].append(y)
+    #     index_counter += 1
+    # print(matrixed_data)
+
+
+
+    matrix = [[y for x, y in v.items()] for k, v in data.items()]
+    header_column = []
     for k, v in data.items():
-        listed_data.append([k, v])
+        header_column.append(k)
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(header_column)
+    i = 0
+    #[list.insert(0, header_column[++i]) for list in matrix]
+    for list in matrix:
+        list.insert(0, header_column[i])
+        i = i+1
+
+
+    #matrix[0].insert(0, header_column)
+
+    print(matrix)
+
+    #matrix_names = [[k for k, v in data.items()],[[y for x, y in v.items()] for k, v in data.items()]]
+
+    #matrix_names2 = [[y for x, y in v.items()] for k, v in data.items()]
+    # for i in range(len(matrix_names2)):
+    #     for k, v in data.items():
+    #         matrix_names2[i].insert(0, k)
+
+    #for k, v in data.items():
+
+    #[{try: list.insert(0, "hallo") except: list+1} for list in matrix_names2]
+
+    #print(matrix_names2)
+
+    matrix_names3 = [[[k , [y for x,y in v.items()]] for k,v in data.items()]]
+    #print(matrix_names3)
+
+
+
+
+
+    # people = {1: {'Name': 'John', 'Age': '27', 'Sex': 'Male'},
+    #           2: {'Name': 'Marie', 'Age': '22', 'Sex': 'Female'}}
+    #
+    # matrix = [][]
+    # for p_id, p_info in people.items():
+    #     print("\nPerson ID:", p_id)
+    #
+    #     for key in p_info:
+    #         print(key + ':', p_info[key])
+
+
+
+    # matrixed_data = []
+    # #for entry in list
+    # for entry in listed_data():
+    #     for k, v in entry.items():
+    #         matrixed_data.append([k, v])
+    # print(matrixed_data)
+
 
     with open('change_log.csv', 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-        writer.writerows(listed_data)
+         writer = csv.writer(f)
+         #writer.writerow(header)
+         writer.writerows(matrix)
+
+def create_plot():
+    df = pandas.read_csv("change_log.csv", header=1)
+    #csv file to panda
+    plt.figure(figsize=(10, 10))
+    plt.ylabel('Absolute Power (log)', fontsize=12)
+    plt.xlabel('Frequencies', fontsize=12)
+    plt.plot(df.columns, df.mean())
+    #ignore key values
+    #just get loc and plot
+
 
 
 def get_change_numbers(path: str):
@@ -48,7 +127,6 @@ def get_change_numbers(path: str):
     for commit in Repository(path).traverse_commits():
         for file in commit.modified_files:
             mod_files.append(file.filename)
-
     file_set = set(mod_files)
     counts = {}
     counter = 0
@@ -61,9 +139,10 @@ def get_change_numbers(path: str):
 
     print(counts)
 
-
 def _count_lines(content: str) -> int:
     lines = content.split('\\n')
     while '' in lines:
         lines.remove('')
     return len(lines)
+
+
